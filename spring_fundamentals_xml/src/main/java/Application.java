f675922dadb9b66dd7206390529f338bf2452429
fdezfdaniel <fdezfdaniel@gmail.com>
2018-10-09 09:21:05 -0600

    using spring dependency injection with setters

--- spring_fundamentals_xml/src/main/java/Application.java
+++ spring_fundamentals_xml/src/main/java/Application.java
@@ -1,13 +1,17 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import fundamentals.spring.pluralsight.service.CustomerService;
-import fundamentals.spring.pluralsight.service.CustomerServiceImpl;
 
 public class Application {
 
 	public static void main(String[] args) {
-		CustomerService service = new CustomerServiceImpl();
-		
-		System.out.println(service.findAll().get(0).getFirstName());
+//		CustomerService service = new CustomerServiceImpl();
 
+		try (ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+			CustomerService service = appContext.getBean("customerService", CustomerService.class);
+
+			System.out.println(service.findAll().get(0).getFirstName());
+		}
 	}
 
 }
--- spring_fundamentals_xml/src/main/java/fundamentals/spring/pluralsight/service/CustomerServiceImpl.java
+++ spring_fundamentals_xml/src/main/java/fundamentals/spring/pluralsight/service/CustomerServiceImpl.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 import fundamentals.spring.pluralsight.model.Customer;
 import fundamentals.spring.pluralsight.repository.CustomerRepository;
-import fundamentals.spring.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+	private CustomerRepository customerRepository; //= new HibernateCustomerRepositoryImpl();
 	
-	/* (non-Javadoc)
-	 * @see fundamentals.spring.pluralsight.service.CustomerService#findAll()
-	 */
 	public List<Customer> findAll(){
 		return customerRepository.findAll();
+	}
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
 	}
 	
 
