33add9042bfc7422a63a5af26e2c248625ce85a5
fdezfdaniel <fdezfdaniel@gmail.com>
2018-10-09 14:51:04 -0600

    end of second module

--- spring_fundamentals_xml_anno/src/main/java/fundamentals/spring/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ spring_fundamentals_xml_anno/src/main/java/fundamentals/spring/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,13 +3,14 @@ package fundamentals.spring.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import fundamentals.spring.pluralsight.model.Customer;
 
+@Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
-	/* (non-Javadoc)
-	 * @see fundamentals.spring.pluralsight.repository.CustomerRepository#findAll()
-	 */
-	public List<Customer> findAll(){
+
+	public List<Customer> findAll() {
 		List<Customer> customersList = new ArrayList<Customer>();
 		Customer customer = new Customer();
 		customer.setFirstName("Daniel");
