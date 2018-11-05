f110fd8eab1cb71d1ca2cba3f00cdb4f98768a81
fdezfdaniel <fdezfdaniel@gmail.com>
2018-11-05 14:07:55 -0700

    using spring data

--- spring_jpa_hibernate/src/main/java/com/pluralsight/repository/ExerciseRepositoryImpl.java
+++ spring_jpa_hibernate/src/main/java/com/pluralsight/repository/ExerciseRepositoryImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.pluralsight.model.Exercise;
 
 @Repository("exerciseRepository")
-public class ExerciseRepositoryImpl implements ExerciseRepository {
+public class ExerciseRepositoryImpl /**implements ExerciseRepository **/ {
 	@PersistenceContext
 	private EntityManager em;
 
