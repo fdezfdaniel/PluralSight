711a3fd5973e714b39cfe45a969c89b2e4577821
fdezfdaniel <fdezfdaniel@gmail.com>
2018-11-05 15:34:19 -0700

    adding spring data jpa

--- spring_data_jpa/ps-guitar-db-master/src/test/java/com/guitar/db/LocationPersistenceTests.java
+++ spring_data_jpa/ps-guitar-db-master/src/test/java/com/guitar/db/LocationPersistenceTests.java
@@ -1,6 +1,7 @@
 package com.guitar.db;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.guitar.db.model.Location;
+import com.guitar.db.repository.LocationJpaRepository;
 import com.guitar.db.repository.LocationRepository;
 
 @ContextConfiguration(locations={"classpath:com/guitar/db/applicationTests-context.xml"})
@@ -22,6 +24,9 @@ import com.guitar.db.repository.LocationRepository;
 public class LocationPersistenceTests {
 	@Autowired
 	private LocationRepository locationRepository;
+	
+	@Autowired
+	private LocationJpaRepository locationJpaRepository;
 
 	@PersistenceContext
 	private EntityManager entityManager;
@@ -62,5 +67,11 @@ public class LocationPersistenceTests {
 		assertEquals(1, arizona.getManufacturers().size());
 		
 		assertEquals("Fender Musical Instruments Corporation", arizona.getManufacturers().get(0).getName());
+	}
+	
+	@Test
+	public void testLocationJpaRepositoryFindAll() {
+		List<Location> locationsList = locationJpaRepository.findAll();
+		assertNotNull(locationsList);
 	}
 }
